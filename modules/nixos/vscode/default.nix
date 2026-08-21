b4058a05a848d04a9f94a40829e8cb6ddd0c0f9d
rafanochi <zawkindev@gmail.com>
2026-08-21 18:02:51 +0500

    refactor: vscode config as a module

---
+++ modules/nixos/vscode/default.nix
@@ -0,0 +1,57 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.vscode;
+  defaultExtensions = with pkgs.vscode-extensions; [
+    # rust
+    rust-lang.rust-analyzer
+    fill-labs.dependi
+
+    # haskell
+    haskell.haskell
+    justusadam.language-haskell
+
+    # nix
+    jnoortheen.nix-ide
+    jeff-hykin.better-nix-syntax
+  ];
+  allExtensions = cfg.extraPackages ++ defaultExtensions;
+in
+with lib;
+{
+  options.vscode = {
+    enable = mkEnableOption "vscode";
+    package = mkPackageOption pkgs "vscodium" { };
+    defaultEditor = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Make the vscode default editor.";
+    };
+
+    extraPackages = mkOption {
+      type = types.listOf types.package;
+      default = [ ];
+      description = "Extra packages for vscode";
+    };
+
+    excludedPackages = mkOption {
+      type = types.listOf types.package;
+      default = [ ];
+      description = "Extra packages for vscode";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.vscode = {
+      enable = true;
+      package = cfg.package;
+      defaultEditor = cfg.defaultEditor;
+
+      extensions = lists.subtractLists cfg.excludedPackages allExtensions;
+    };
+  };
+}
